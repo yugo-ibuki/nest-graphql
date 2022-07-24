495be44ba2a9b5ae3c73e8cf302ce7fe8178e013
yugo-ibuki <y.ibuki91@gmail.com>
2022-07-25 00:13:50 +0900

    class-validate追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './components/posts/posts.module';
 import * as path from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/environments/env-validator';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+      validate,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
---
+++ backend/src/config/environments/env-validator.ts
@@ -0,0 +1,51 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  validateSync,
+} from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+enum NodeEnvEnum {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+}
+
+/**
+ * ①
+ * バリデーションしたい環境変数がある場合はここに記載してください。
+ * バリデーションに失敗するとアプリケーションは起動しません。
+ */
+export class EnvValidator {
+  @IsEnum(NodeEnvEnum)
+  NODE_ENV: NodeEnvEnum;
+
+  @IsNumber()
+  PORT = 3333; // デフォルト値も指定できる
+
+  @IsNotEmpty()
+  @IsString()
+  DATABASE_URL: string;
+}
+
+/**
+ * ②
+ * @param config バリデーション対象の Record<string, any>。今回は .env.development.local と 環境変数が合体したもの
+ * @returns バリデーション済の Record<string, any>
+ */
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToClass(EnvValidator, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
