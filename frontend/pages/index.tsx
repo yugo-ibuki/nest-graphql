a97b213674c9f1857a59dc933c7d1fe02ffa55a3
yugo-ibuki <y.ibuki91@gmail.com>
2022-07-24 22:45:26 +0900

    urqlのサンプル追加

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from "next";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { urqlClient } from '../libs/gql-requests';
+import { gql } from 'urql';
 
-const Home: NextPage = () => {
+type Props = {
+  posts: {
+    id: string;
+    title: string;
+  }[];
+};
+
+const Home: NextPage<Props> = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,40 +26,13 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {props.posts.map((post) => (
+            <li className={styles.title} key={post.id}>
+              id: {post.id} title: {post.title}
+            </li>
+          ))}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -68,5 +50,33 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  try {
+    const client = await urqlClient();
+
+    // 変数なしでGraphQL呼び出し
+    const postsQuery = gql`
+      query {
+        posts {
+          id
+          title
+        }
+      }
+    `;
+    const result = await client.query(postsQuery, {}).toPromise();
+
+    return {
+      props: {
+        posts: result.data.posts,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      notFound: true,
+    };
+  }
+};
 
 export default Home
