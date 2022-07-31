25fd84b58d98968dac56b9f7e7e9700aa622f455
yugo-ibuki <y.ibuki91@gmail.com>
2022-07-31 23:37:51 +0900

    portのバリデーションを追加

--- backend/src/components/posts/post.resolver.ts
+++ backend/src/components/posts/post.resolver.ts
@@ -1,13 +1,24 @@
+import { ConfigService } from '@nestjs/config';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/post.model';
 import { PostService } from './post.service';
 
 @Resolver((of) => PostModel)
 export class PostResolver {
-  constructor(private PostService: PostService) {}
+  constructor(
+    private configService: ConfigService,
+    private PostService: PostService,
+  ) {}
 
   @Query(() => [PostModel], { name: 'posts', nullable: true })
   async posts() {
     return this.PostService.getPosts();
+  }
+
+  @Query(() => String)
+  helloConfiguration() {
+    const nodeEnv = this.configService.get<string>('NODE_ENV'); // development （.env.development.localのもの）
+    const databaseUrl = this.configService.get<string>('DATABASE_URL'); // postgresql:/... （.env.development.localのもの）
+    const microCmsKey = this.configService.get<string>('MICRO_CMS_KEY'); // 1234567890（環境変数のもの）
   }
 }
--- backend/src/components/posts/posts.module.ts
+++ backend/src/components/posts/posts.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [PostResolver, PostService],
+  providers: [PostResolver, PostService, ConfigService],
 })
 export class PostsModule {}
